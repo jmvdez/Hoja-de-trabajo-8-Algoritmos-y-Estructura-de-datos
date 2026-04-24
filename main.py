2e3e480579eccf68bdb218bc79c0d67a401257d3
Kenneth Maldonado <maldonadovaldez900@gmail.com.com>
2026-04-23 18:25:07 -0600

    Paso 4: Implementación BST completa y escenario A básico

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
+import random
+
 class BSTNode:
     def __init__(self, key):
         self.key = key
         self.left = None
         self.right = None
 
-
 class BST:
     def __init__(self):
         self.root = None
 
-    def search(self, key):
+    def insert(self, key):
+        if not self.root:
+            self.root = BSTNode(key)
+            return
+        
         current = self.root
-        iterations = 0
-
-        while current:
-            iterations += 1
-            if key == current.key:
-                return current, iterations
-            elif key < current.key:
-                current = current.left
+        while True:
+            if key < current.key:
+                if current.left:
+                    current = current.left
+                else:
+                    current.left = BSTNode(key)
+                    return
             else:
-                current = current.right
+                if current.right:
+                    current = current.right
+                else:
+                    current.right = BSTNode(key)
+                    return
 
-        return None, iterations
+
+def escenario_A_BST():
+    bst = BST()
+
+    values = [random.randint(1, 10000) for _ in range(1000)]
+
+    for v in values:
+        bst.insert(v)
+
+    searches = random.sample(values, 100)
+
+    total = 0
+    for s in searches:
+        _, it = bst.search(s)
+        total += it
+
+    return total / 100
