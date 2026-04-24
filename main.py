c20a5a5506603ba7141d97b595bd11a77b69eebc
Kenneth Maldonado <maldonadovaldez900@gmail.com.com>
2026-04-23 18:15:32 -0600

    Paso 2: UML y clases base implementadas

---
+++ main.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+
+class SplayTree:
+    def __init__(self):
+        self.root = None
+
+
+class RedBlackTree:
+    def __init__(self):
+        self.root = None
